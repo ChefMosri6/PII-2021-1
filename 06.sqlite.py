414785185fc7c8405444f94849abb741ffc651c5
Mosri <mosrichristian@gmail.com>
2021-04-15 20:35:53 -0500

    Change

---
+++ 06.sqlite.py
@@ -0,0 +1,13 @@
+from bottle import route, run, template, static_file, request, get
+import sqlite3
+import sys
+
+
+@route('/')
+def index():
+    # database connection
+    connection = sqlite3.connect('./sqlite-tools-win32-x86-3350200/univa.db')
+    mycursor = connection.cursor()
+    raws = mycursor.execute('SELECT * FROM students;')
+    return template('index.tpl', raws=raws)
+    mycursor.close()
