95bcba012301f3e9a977755b3922879c66287604
Mosri <mosrichristian@gmail.com>
2021-04-17 18:18:24 -0500

    sql

--- 06.sqlite.py
+++ 06.sqlite.py
@@ -9,7 +9,7 @@ def index():
     connection = sqlite3.connect('./sqlite/univa.db')
     mycursor = connection.cursor()
     rows = mycursor.execute('SELECT * FROM students;')
-    return template('index.tpl', rows=rows, name="UNIVA.DB-SQlite" )
+    return template('index_new_copy.tpl', rows=rows, name="UNIVA.DB-SQlite" )
     mycursor.close()
     
 run(host= 'localhost', port = 3000, debug = True, reloader = True)
